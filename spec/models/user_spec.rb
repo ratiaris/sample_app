beda72872e91a19841ab32ed16fad18cce3844a5
Benjamin Ratiarisolo <benjamin.ratiarisolo@fr.ibm.com>
2014-08-23 18:22:36 +0200

    Finalized following users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -167,15 +167,26 @@ describe User do
             let (:unfollowed_post) do
                 FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
             end
+            let(:followed_user) { FactoryGirl.create :user }
 
-            its(:feed) { should include(newer_micropost) }
-            its(:feed) { should include(older_micropost) }
-            its(:feed) { should_not include(unfollowed_post) }
+            before do
+                @user.follow! followed_user
+                3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+            end
+
+            its(:feed) { should include newer_micropost }
+            its(:feed) { should include older_micropost }
+            its(:feed) { should_not include unfollowed_post }
+            its(:feed) do
+                followed_user.microposts.each do | micropost |
+                    should include micropost
+                end
+            end
         end
     end
 
     describe "following" do
-        let(:other_user) { FactoryGirl.create(:user) }
+        let(:other_user) { FactoryGirl.create :user }
         before do
             @user.save
             @user.follow!(other_user)
